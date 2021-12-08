e5be4eb814d1de40eb9873786bad92d83f2c955c
faisal.khan <faisal.khan@ugamsolutions.com>
2021-12-08 22:53:58 +0530

    image upload added for gift

--- App/routes/gift.js
+++ App/routes/gift.js
@@ -1,7 +1,8 @@
+const { upload } = require('./../middlewares/multer');
 const giftController = require('./../controllers/giftController');
 
 module.exports.setRouter = (app) =>{
     app.get('/get-all-gift', giftController.getAllGifts);
 
-    app.post('/add-gift', giftController.addGift);
+    app.post('/add-gift', upload.single('giftImage') ,giftController.addGift);
 }
