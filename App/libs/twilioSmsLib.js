8531a176b680698a8f8e9e3b030084a45578d965
faisal.khan <faisal.khan@ugamsolutions.com>
2021-12-25 20:59:19 +0530

    log added

--- App/libs/twilioSmsLib.js
+++ App/libs/twilioSmsLib.js
@@ -1,5 +1,5 @@
 const clinet = require('twilio')(process.env.accountSid, process.env.authToken);
-
+console.log(process.env.accountSid, process.env.authToken)
 module.exports.createTwilioOtp = async (mobileNumber) => {
     let verifyDetails = await clinet.verify.services.create({ friendlyName: 'Dazo Live OTP', codeLength: 4 })
     let createOtp = await clinet.verify.services(verifyDetails.sid)
